0c1b6e8ac60848f5af5618c08df556ca7a1eb34c
Alden <aldenwangf2e@gmail.com>
2025-03-22 08:06:25 +0800

    BMI_fundation => MVVM

---
+++ BMICalculator/Model/BMIModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BMICalculator.Models
+{
+    public class BMIModel : INotifyPropertyChanged
+    {
+        private double _weight;
+        private double _height;
+        private double _bmi;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Weight
+        {
+            get => _weight;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("體重必須大於 0");
+                _weight = value;
+                OnPropertyChanged(nameof(Weight));
+                CalculateBMI();
+            }
+        }
+
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("身高必須大於 0");
+                _height = value;
+                OnPropertyChanged(nameof(Height));
+                CalculateBMI();
+            }
+        }
+
+        public double BMI
+        {
+            get => _bmi;
+            private set
+            {
+                _bmi = value;
+                OnPropertyChanged(nameof(BMI));
+            }
+        }
+
+        private void CalculateBMI()
+        {
+            if (_weight > 0 && _height > 0)
+            {
+                double heightInMeters = _height / 100.0;
+                BMI = _weight / (heightInMeters * heightInMeters);
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ BMICalculator/ViewModel/BMIViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+using BMICalculator.Models;
+
+namespace BMICalculator.ViewModels
+{
+    public class BMIViewModel
+    {
+        public BMIModel BMIModel { get; set; }
+        public ICommand CalculateCommand { get; }
+
+        public BMIViewModel()
+        {
+            BMIModel = new BMIModel();
+            CalculateCommand = new RelayCommand(CalculateBMI);
+        }
+
+        private void CalculateBMI(object parameter)
+        {
+            try
+            {
+                BMIModel.Weight = Convert.ToDouble(parameter as string);
+                BMIModel.Height = Convert.ToDouble(parameter as string);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"輸入錯誤: {ex.Message}");
+            }
+        }
+    }
+}
